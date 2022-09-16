242c2b9220eebb6dffd77800cfc29ecdcb112c48
furukoshi <ykh2135013@stu.o-hara.ac.jp>
2022-09-16 11:37:13 +0900

    9/16 アクセス権限設定完了

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 import  logging
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.urls import reverse_lazy
 
@@ -12,7 +12,16 @@ from .forms import InquiryForm, DiaryCreateForm
 
 from django.contrib import messages
 
+from django.shortcuts import get_object_or_404
+
 logger = logging.getLogger(__name__)
+
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        diary = get_object_or_404(Diary, pk=self.kwargs['pk'])
+        return self.request.user == diary.user
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -37,7 +46,7 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
 
-class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+class DiaryDetailView(LoginRequiredMixin, OnlyYouMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
 
@@ -58,7 +67,7 @@ class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
         messages.error(self.request, "日記の作成に失敗しました。")
         return super().form_invalid(form)
 
-class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+class DiaryUpdateView(LoginRequiredMixin, OnlyYouMixin, generic.UpdateView):
     model = Diary
     template_name = 'diary_update.html'
     form_class = DiaryCreateForm
@@ -74,7 +83,7 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
         messages.error(self.request, "日記の更新に失敗しました。")
         return super().form_invalid(form)
 
-class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+class DiaryDeleteView(LoginRequiredMixin, OnlyYouMixin, generic.DeleteView):
     model = Diary
     template_name = 'diary_delete.html'
     success_url = reverse_lazy('diary:diary_list')
